ce795743a08dc220aebf1aa1d4988b0b3915f8e2
bourraOmar <bwrtmr@gmail.com>
2025-03-26 13:58:28 +0000

    update migration seats and  reservation

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Seat;
+use App\Models\Session;
 
 class Room extends Model
 {
     use HasFactory;
-    protected $fillable = ['type'];
+    protected $fillable = ['name', 'capacity'];
+
+    public function seats()
+    {
+        return $this->hasMany(Seat::class);
+    }
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
 }
