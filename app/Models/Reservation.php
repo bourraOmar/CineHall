406cde57c33ab78f8f9f470a837da5c8cf53b8a4
bourraOmar <bwrtmr@gmail.com>
2025-04-04 20:14:01 +0000

    update reservation module

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Session;
+use App\Models\Seat;
 
 class Reservation extends Model
 {
-    use HasFactory;
+    protected $fillable = ['user_id', 'session_id', 'seat_id', 'type'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function session()
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    public function seats()
+    {
+        return $this->belongsToMany(Seat::class);
+    }
 }
